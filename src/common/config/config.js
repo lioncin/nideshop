4635018cc600999e05b8c1bd4787d2e640d119fe
lioncin <linxing1984@gmail.com>
2023-08-22 10:31:25 +0800

    账号更新

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -2,8 +2,8 @@
 module.exports = {
 	default_module: 'api',
 	weixin: {
-		appid: 'wx5c06bc23687d4828', // 小程序 appid
-		secret: '5641f60df9759f2cbebab08878d9f3d9', // 小程序密钥
+		appid: '', // 小程序 appid
+		secret: '', // 小程序密钥
 		mch_id: '', // 商户帐号ID
 		partner_key: '', // 微信支付密钥
 		notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify

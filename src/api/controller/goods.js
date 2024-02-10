2f77b77d6e0d6271c0dd630ab866a36ee67ec6d3
linxing <linxing@morongai.com>
2024-02-10 23:40:40 +0800

    fx

--- src/api/controller/goods.js
+++ src/api/controller/goods.js
@@ -91,94 +91,100 @@ module.exports = class extends Base {
     });
   }
 
+  async listAction() {
+    const model = this.model('goods');
+    const list = await model.limit(100).select();
+    return this.success(list);
+  }
+
   /**
    * 获取商品列表
    * @returns {Promise.<*>}
    */
-  async listAction() {
-    const categoryId = this.get('categoryId');
-    const brandId = this.get('brandId');
-    const keyword = this.get('keyword');
-    const isNew = this.get('isNew');
-    const isHot = this.get('isHot');
-    const page = this.get('page');
-    const size = this.get('size');
-    const sort = this.get('sort');
-    const order = this.get('order');
+  // async listAction() {
+  //   const categoryId = this.get('categoryId');
+  //   const brandId = this.get('brandId');
+  //   const keyword = this.get('keyword');
+  //   const isNew = this.get('isNew');
+  //   const isHot = this.get('isHot');
+  //   const page = this.get('page');
+  //   const size = this.get('size');
+  //   const sort = this.get('sort');
+  //   const order = this.get('order');
 
-    const goodsQuery = this.model('goods');
+  //   const goodsQuery = this.model('goods');
 
-    const whereMap = {};
-    if (!think.isEmpty(isNew)) {
-      whereMap.is_new = isNew;
-    }
+  //   const whereMap = {};
+  //   if (!think.isEmpty(isNew)) {
+  //     whereMap.is_new = isNew;
+  //   }
 
-    if (!think.isEmpty(isHot)) {
-      whereMap.is_hot = isHot;
-    }
+  //   if (!think.isEmpty(isHot)) {
+  //     whereMap.is_hot = isHot;
+  //   }
 
-    if (!think.isEmpty(keyword)) {
-      whereMap.name = ['like', `%${keyword}%`];
-      // 添加到搜索历史
-      await this.model('search_history').add({
-        keyword: keyword,
-        user_id: this.getLoginUserId(),
-        add_time: parseInt(new Date().getTime() / 1000)
-      });
-    }
+  //   if (!think.isEmpty(keyword)) {
+  //     whereMap.name = ['like', `%${keyword}%`];
+  //     // 添加到搜索历史
+  //     await this.model('search_history').add({
+  //       keyword: keyword,
+  //       user_id: this.getLoginUserId(),
+  //       add_time: parseInt(new Date().getTime() / 1000)
+  //     });
+  //   }
 
-    if (!think.isEmpty(brandId)) {
-      whereMap.brand_id = brandId;
-    }
+  //   if (!think.isEmpty(brandId)) {
+  //     whereMap.brand_id = brandId;
+  //   }
 
-    // 排序
-    let orderMap = {};
-    if (sort === 'price') {
-      // 按价格
-      orderMap = {
-        retail_price: order
-      };
-    } else {
-      // 按商品添加时间
-      orderMap = {
-        id: 'desc'
-      };
-    }
+  //   // 排序
+  //   let orderMap = {};
+  //   if (sort === 'price') {
+  //     // 按价格
+  //     orderMap = {
+  //       retail_price: order
+  //     };
+  //   } else {
+  //     // 按商品添加时间
+  //     orderMap = {
+  //       id: 'desc'
+  //     };
+  //   }
 
-    // 筛选的分类
-    let filterCategory = [{
-      'id': 0,
-      'name': '全部',
-      'checked': false
-    }];
+  //   // 筛选的分类
+  //   let filterCategory = [{
+  //     'id': 0,
+  //     'name': '全部',
+  //     'checked': false
+  //   }];
 
-    const categoryIds = await goodsQuery.where(whereMap).getField('category_id', 10000);
-    if (!think.isEmpty(categoryIds)) {
-      // 查找二级分类的parent_id
-      const parentIds = await this.model('category').where({id: {'in': categoryIds}}).getField('parent_id', 10000);
-      // 一级分类
-      const parentCategory = await this.model('category').field(['id', 'name']).order({'sort_order': 'asc'}).where({'id': {'in': parentIds}}).select();
+  //   const categoryIds = await goodsQuery.where(whereMap).getField('category_id', 10000);
+  //   if (!think.isEmpty(categoryIds)) {
+  //     // 查找二级分类的parent_id
+  //     const parentIds = await this.model('category').where({id: {'in': categoryIds}}).getField('parent_id', 10000);
+  //     // 一级分类
+  //     const parentCategory = await this.model('category').field(['id', 'name']).order({'sort_order': 'asc'}).where({'id': {'in': parentIds}}).select();
 
-      if (!think.isEmpty(parentCategory)) {
-        filterCategory = filterCategory.concat(parentCategory);
-      }
-    }
+  //     if (!think.isEmpty(parentCategory)) {
+  //       filterCategory = filterCategory.concat(parentCategory);
+  //     }
+  //   }
 
-    // 加入分类条件
-    if (!think.isEmpty(categoryId) && parseInt(categoryId) > 0) {
-      whereMap.category_id = ['in', await this.model('category').getCategoryWhereIn(categoryId)];
-    }
+  //   // 加入分类条件
+  //   if (!think.isEmpty(categoryId) && parseInt(categoryId) > 0) {
+  //     whereMap.category_id = ['in', await this.model('category').getCategoryWhereIn(categoryId)];
+  //   }
 
-    // 搜索到的商品
-    const goodsData = await goodsQuery.where(whereMap).field(['id', 'name', 'list_pic_url', 'retail_price']).order(orderMap).page(page, size).countSelect();
-    goodsData.filterCategory = filterCategory.map(function(v) {
-      v.checked = (think.isEmpty(categoryId) && v.id === 0) || v.id === parseInt(categoryId);
-      return v;
-    });
-    goodsData.goodsList = goodsData.data;
+  //   // 搜索到的商品
+  //   const goodsData = await goodsQuery.where(whereMap).field(['id', 'name', 'list_pic_url', 'retail_price']).order(orderMap).page(page, size).countSelect();
+  //   goodsData.filterCategory = filterCategory.map(function(v) {
+  //     v.checked = (think.isEmpty(categoryId) && v.id === 0) || v.id === parseInt(categoryId);
+  //     return v;
+  //   });
+  //   goodsData.goodsList = goodsData.data;
 
-    return this.success(goodsData);
-  }
+  //   return this.success(goodsData);
+  // }
 
   /**
    * 商品列表筛选的分类列表
